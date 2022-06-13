204e02ec6f0ba9b24a96315b5c4374de2b6870df
bishar08 <abubushayra@gmail.com>
2022-06-13 09:28:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+// serve static files from public
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.send('Hello world!')
+})
+
+app.listen(3000, function () {
+  console.log('Running on port: 3000')
+})
